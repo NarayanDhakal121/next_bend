a56765e0a1f5e1103706725eef02619d911d5956
NarayanDhakal121 <www.narine121@gmail.com>
2024-04-10 02:55:13 +0545

    COMMIT

--- src/app/api/users/login/route.ts
+++ src/app/api/users/login/route.ts
@@ -1,67 +1,62 @@
-import bcryptjs from 'bcryptjs';
-import User from '../../../../models/userModel';
-import { NextRequest, NextResponse} from 'next/server';
+import bcryptjs from "bcryptjs";
+import User from "../../../../models/userModel";
+import { NextRequest, NextResponse } from "next/server";
 import { connectDb } from "@/dbConfig/dbConfig";
-import jwt from 'jsonwebtoken'; 
+import jwt from "jsonwebtoken";
 
-connectDb()
+connectDb();
 
-export async function POST(request: NextRequest){
-    try {
+export async function POST(request: NextRequest) {
+  try {
+    // 1) take the data from the user
+    const reqBody = await request.json();
+    const { email, password } = reqBody;
+    console.log(reqBody);
 
-      // 1) take the data from the user
-      const reqBody = await request.json();
-      const{email,password} = reqBody;
-      console.log(reqBody)
+    // 2) checks wheather the user exists or not.
+    const user = await User.findOne({ email });
 
-      // 2) checks wheather the user exists or not
-     const user=  await User.findOne({ email});
+    if (!user) {
+      return NextResponse.json({ error: "User not found" }, { status: 400 });
+    }
 
-     if(!user) {
-    return NextResponse.json({error: "User not found"}, {status: 400})
+    console.log("User exists");
 
-     }
+    // 3) check wheather the provided credintials are valid or not
+    const validPassword = await bcryptjs.compare(password, user.password);
 
-     console.log("User exists");
+    if (!validPassword) {
+      return NextResponse.json(
+        { error: "Check your creanditials" },
+        { status: 400 }
+      );
+    }
 
+    // jwt token
 
-     // 3) check wheather the provided credintials are valid or not
-   const validPassword =  await bcryptjs.compare(password, user.password)
+    const tokenData = {
+      id: user._id,
+      username: user.username,
+      email: user.email,
+    };
 
-   if(!validPassword) {
+    // 4) use of jwt for comparision
 
-    return NextResponse.json({error:"Check your creanditials"}, {status: 400})
+    const token = jwt.sign(tokenData, "ghfdffcgvhghd", { expiresIn: "1d" });
 
-   }
+    const response = NextResponse.json({
+      message: "logged in suscess",
+      suscess: true,
+    });
 
-   // jwt token 
- 
+    // setting the cookie
 
- const tokenData = {
-  id: user._id,
-  username: user.username,
-  email: user.email,
-   }
+    response.cookies.set("token", token, {
+      httpOnly: true,
+    });
 
-     // 4) use of jwt for comparision
-
-   const token = jwt.sign(tokenData, 'ghfdffcgvhghd', { expiresIn: '1d' })
-
-  const response = NextResponse.json({message: "logged in suscess",
-    suscess:true
-   })
-
-   // setting the cookie
-
-   response.cookies.set("token", token, {
-    httpOnly: true
-   })
-
-return response
-        
-    } catch (error:any) {
-        return NextResponse.json({error: error.message},{status: 500})
-        
-    } 
+    return response;
+  } catch (error: any) {
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
 }
-
--- src/helpers/mailer.ts
+++ src/helpers/mailer.ts
@@ -1,51 +1,54 @@
-import User from '@/models/userModel';
-import nodemailer from 'nodemailer';
-import bcryptjs from 'bcryptjs';
+import User from "@/models/userModel";
+import nodemailer from "nodemailer";
+import bcryptjs from "bcryptjs";
 
 export const sendEmail = async ({ email, emailType, userId }: any) => {
-    try {
-        const hashedToken = await bcryptjs.hash(userId.toString(), 10);
+  try {
+    const hashedToken = await bcryptjs.hash(userId.toString(), 10);
 
-        if (emailType === 'VERIFY') {
-            await User.findByIdAndUpdate(userId, {
-                $set: {
-                    verifyToken: hashedToken,
-                    verifyTokenExpiry: Date.now() + 3600000
-                }
-            });
-        } else if (emailType === 'RESET') {
-            await User.findByIdAndUpdate(userId, {
-                $set: {
-                    forgotPasswordToken: hashedToken,
-                    forgotPasswordTokenExpiry: Date.now() + 3600000
-                }
-            });
+    if (emailType === "VERIFY") {
+      await User.findByIdAndUpdate(userId, {
+        $set: {
+          verifyToken: hashedToken,
+          verifyTokenExpiry: Date.now() + 3600000,
+        },
+      });
+    } else if (emailType === "RESET") {
+      await User.findByIdAndUpdate(userId, {
+        $set: {
+          forgotPasswordToken: hashedToken,
+          forgotPasswordTokenExpiry: Date.now() + 3600000,
+        },
+      });
+    }
+
+    var transporter = nodemailer.createTransport({
+        host: "sandbox.smtp.mailtrap.io",
+        port: 2525,
+        auth: {
+          user: "4608b662d2e461",  //confidenciality - _ -
+          pass: "65371c47693169" // confidenciality **
         }
+      });
 
-        const transporter = nodemailer.createTransport({
-            host: "smtp.ethereal.email",
-            port: 587,
-            secure: false, // Use `true` for port 465, `false` for all other ports
-            auth: {
-                user: "maddison53@ethereal.email",
-                pass: "jn7jnAPss4f63QBp6D",
-            },
-        });
-
-        const mailOptions = {
-            from: 'narine@narine.ai', // sender address
-            to: email,
-            subject: emailType === 'VERIFY' ? "Verify your email" : "Reset your password",
-            html: `<p>Click <a href="${process.env.Domain}/verifyemail?token=${hashedToken}">Here</a> to ${emailType === "VERIFY" ? "verify your email" : "reset your password"}
+    const mailOptions = {
+      from: "narine@narine.ai", // sender address
+      to: email,
+      subject:
+        emailType === "VERIFY" ? "Verify your email" : "Reset your password",
+      html: `<p>Click <a href="${
+        process.env.Domain
+      }/verifyemail?token=${hashedToken}">Here</a> to ${
+        emailType === "VERIFY" ? "verify your email" : "reset your password"
+      }
             or copy and paste the link below in the browser
             <br>${process.env.domain}/verifyemail?token=${hashedToken}
             </p>`, // html body
-        };
+    };
 
-        const mailResponse = await transporter.sendMail(mailOptions);
-        return mailResponse;
-
-    } catch (error:any) {
-        throw new Error(error.message);
-    }
+    const mailResponse = await transporter.sendMail(mailOptions);
+    return mailResponse;
+  } catch (error: any) {
+    throw new Error(error.message);
+  }
 };
